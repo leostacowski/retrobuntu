2200f8685d7fda55a73b6fd95ae91bd6c4dc8fb6
leostacowskicompasso <111765225+leostacowskicompasso@users.noreply.github.com>
2025-03-26 20:42:30 -0300

    feat: added adguardgome docker fies

--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-service xrdp stop
-useradd -m $(printenv SSH_USER)
-echo "$(printenv SSH_USER):$(printenv SSH_PASS)" | chpasswd
-usermod -aG sudo $(printenv SSH_USER)
-service xrdp start
-sleep infinity
